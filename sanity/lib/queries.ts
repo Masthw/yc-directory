e892d490e9da3e78242bcf2834be39e53f2b86fe
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-06-24 15:22:07 -0300

    :sparkles: feat: create Startup form

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -27,6 +27,18 @@ export const STARTUP_BY_ID_QUERY =
   pitch
 }`);
 
-export const STARTUP_VIEW_QUERY = defineQuery(`*[_type == "startup" && _id == $id][0]{
+export const STARTUP_VIEW_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
   _id, views
-}`)
+}`);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY =
+  defineQuery(`*[_type == "author" && id == $id][0]{
+  _id,
+  id,
+  name,
+  username,
+  email,
+  image,
+  bio
+}`);

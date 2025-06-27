261c672ad80b74b923faf7641024ef132c1cf4dc
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-06-27 08:46:09 -0300

    :sparkles: feat: implement server actions to create startups

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -9,6 +9,7 @@ import { formSchema } from "@/lib/validation";
 import { z } from "zod";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -26,13 +27,12 @@ const StartupForm = () => {
       };
       await formSchema.parseAsync(formValues);
       console.log(formValues);
-      //const result = await createIdea(prevState, formData, pitch);
-      //console.log(result);
-
-      if(result.status === "SUCCESS") {
+      const result = await createPitch(prevState, formData, pitch);
+      if (result.status === "SUCCESS") {
         toast.success("Your startup pitch has been created successfully.");
+        router.push(`/startup/${result._id}`);
       }
-      router.push(`/startup/${result.id}`);
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors;
